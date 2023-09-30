9e843c5d5e030511447d8ba6ca6483d7018b418a
unknown <mouatacim.med@gmail.com>
2023-09-30 14:41:30 +0200

    added basic javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('first')
+const input = document.querySelector("input")
+const btn = document.querySelector("button")
+const taskList = document.querySelector("ul")
+
+class Task{
+    constructor(id, taskName, doneBool){
+        this.id = id
+        this.taskName = taskName
+        this.doneBool = doneBool
+    }
+}
