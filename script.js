9dea26404ec95f0ea5b494517b48155849218ee4
unknown <mouatacim.med@gmail.com>
2023-10-02 02:09:30 +0200

    added basic todo capability

--- script.js
+++ script.js
@@ -8,4 +8,21 @@ class Task{
         this.taskName = taskName
         this.doneBool = doneBool
     }
+    generateNextId(){
+        return this.id++
+    }
 }
+
+function addTask(){
+    if(input.value != ""){
+        let task = new Task(0, input.value, false)
+        const li = document.createElement("li")
+        li.textContent = task.taskName
+        li.dataset.id = task.id
+        taskList.appendChild(li)
+    }else{
+        console.error("cant add empty task")
+    }
+}
+
+btn.addEventListener("click", addTask)
