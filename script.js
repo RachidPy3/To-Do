249c258401116d4cffd5e4ca10cd56cf9f87a9f6
unknown <mouatacim.med@gmail.com>
2023-10-02 19:51:30 +0200

    implemented an id system

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-const input = document.querySelector("input")
-const btn = document.querySelector("button")
-const taskList = document.querySelector("ul")
-
 class Task{
     constructor(id, taskName, doneBool){
         this.id = id
         this.taskName = taskName
         this.doneBool = doneBool
     }
-    generateNextId(){
-        return this.id++
-    }
+}
+
+const input = document.querySelector("input")
+const btn = document.querySelector("button")
+const taskList = document.querySelector("ul")
+
+let id = 0
+
+function createTask(){
+    let task = new Task(id, input.value, false)
+    const li = document.createElement("li")
+    li.textContent = task.taskName
+    li.dataset.id = task.id
+    return li
 }
 
 function addTask(){
     if(input.value != ""){
-        let task = new Task(0, input.value, false)
-        const li = document.createElement("li")
-        li.textContent = task.taskName
-        li.dataset.id = task.id
-        taskList.appendChild(li)
+        let task = createTask()
+        taskList.appendChild(task)
+        id++
     }else{
         console.error("cant add empty task")
     }
